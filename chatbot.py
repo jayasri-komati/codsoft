654df4c713cef5ddffded59875d8cf9c449f4156
jayasri-komati <jayasrikomati434@gmail.com>
2026-06-28 18:06:26 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,34 @@
+print("=================================")
+print("      AI RULE-BASED CHATBOT")
+print("=================================")
+
+print("Type 'bye' to exit.\n")
+
+while True:
+
+    user = input("You : ").lower()
+
+    if user == "hello":
+        print("Bot : Hello! Welcome.")
+
+    elif user == "hi":
+        print("Bot : Hi! How are you?")
+
+    elif user == "how are you":
+        print("Bot : I am doing great!")
+
+    elif user == "what is your name":
+        print("Bot : I am CodSoft AI Chatbot.")
+
+    elif user == "who created you":
+        print("Bot : I was created using Python.")
+
+    elif user == "thank you":
+        print("Bot : You're welcome!")
+
+    elif user == "bye":
+        print("Bot : Goodbye! Have a nice day.")
+        break
+
+    else:
+        print("Bot : Sorry, I don't understand.")
